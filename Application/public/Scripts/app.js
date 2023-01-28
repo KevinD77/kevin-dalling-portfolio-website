a2e0dd049fbf0e39b583ead691ede1e2d739c1a7
KevinD77 <kevindalling@hotmail.com>
2023-01-28 01:57:15 -0500

    initial commit

---
+++ Application/public/Scripts/app.js
@@ -0,0 +1,9 @@
+// IIFE - Immediately Invoked Function Express
+(function(){
+    function Start()
+    {
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load",Start)
+})();
