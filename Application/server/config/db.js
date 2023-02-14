951a5835a9fa4fe75255f9bba2c4cc6037c809be
KevinD77 <kevindalling@hotmail.com>
2023-02-13 23:28:53 -0500

    updated DB URI to work with MongoDB Atlas

--- Application/server/config/db.js
+++ Application/server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contact_db"
+    //"URI": "mongodb://localhost/contact_db"
+    "URI": "mongodb+srv://kdalling:Y3FUbQQYsQB4SUeg@mongodbserver.yvyg8yb.mongodb.net/contact_db?retryWrites=true&w=majority"
 }
